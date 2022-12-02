e9dc18510947d61afa54846ad33458288c1a0783
David Bass <davidbass@rtonational.com>
2022-12-02 15:48:33 -0500

    Add files via upload

---
+++ Multiplication Of Three numbers/Multiplication Of Three numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Multiplication_Of_Three_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number to multiply: ");
+            string num1 = Console.ReadLine();
+            Console.Write("Input the second number to multiply: ");
+            string num2 = Console.ReadLine();
+            Console.Write("Input the third number to multiply: ");
+            string num3 = Console.ReadLine();
+            int sum = int.Parse(num1) * int.Parse(num2) * int.Parse(num3);
+            Console.WriteLine();
+            Console.WriteLine(num1 + " x " + num2 + " x " + num3 + " = "+ sum);
+        }
+    }
+}
