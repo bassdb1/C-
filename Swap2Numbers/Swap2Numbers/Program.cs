f9fc071ec114b9ea5b6d5580788feda210da32b4
David Bass <davidbass@rtonational.com>
2022-12-02 15:28:49 -0500

    Add files via upload

---
+++ Swap2Numbers/Swap2Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Swap2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Input the First Number: ");
+            string num1 = Console.ReadLine();
+            Console.Write("Input the Second Number: ");
+            string num2 = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("AFter Swapping:");
+            Console.WriteLine("First Number: " + num2);
+            Console.WriteLine("Second Number: " + num1);
+
+        }
+    }
+}
