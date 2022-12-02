1c48efef81c08a918ea5caf44c5e4e49d1bf0383
David Bass <davidbass@rtonational.com>
2022-12-02 15:11:42 -0500

    Add files via upload

---
+++ Operations of Numbers/Operations of Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operations_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = -1;
+            int num2 = 4;
+            int num3 = 7;
+            int num4 = 6;
+            int num5 = 14;
+            int num6 = -4;
+            int num7 = 11;
+            int num8 = 2;
+            int num9 = 1;
+            int num10 = 35;
+            int num11 = 5;
+            int num12 = 15;
+            int sum = 0;
+            
+            Console.WriteLine(sum = num1 + num2 * num4 );
+            sum = 0;
+            Console.WriteLine(sum = (num10 + num11) % num3);
+            sum = 0;
+            Console.WriteLine (sum = num5 + num6 * num4 / num7);
+            sum = 0;
+            Console.WriteLine(sum = num8 + num12 / num4 * num9 - num3 %2);
+            Console.Read();
+        }
+    }
+}
