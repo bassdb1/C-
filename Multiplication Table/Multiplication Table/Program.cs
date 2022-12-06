967d17bfe14879d5ac9b6cb6da093e41d3a32a61
David Bass <davidbass@rtonational.com>
2022-12-06 13:26:21 -0500

    Add files via upload

---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get input from user
+
+            var length = 11;
+            Console.Write("Enter the number: ");
+            string num1 = Console.ReadLine();
+            int number = int.Parse(num1);
+
+            // Print out the Multiplication Table
+            Console.WriteLine();
+            for (int i = 0; i < length; i++)
+            {
+                int sum = number * i;
+                Console.WriteLine(num1 + " * " + i + " = " + sum);
+            }
+        }
+    }
+}
