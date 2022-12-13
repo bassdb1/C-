fd4084874e0ff0d20f84067f5a5ca90bf41a80c6
David Bass <davidbass@rtonational.com>
2022-12-13 15:20:54 -0500

    Add files via upload

---
+++ Math_Average/Math_Average/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Math_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get input from user
+
+            
+            Console.Write("Enter the First number: ");
+            var number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Second number: ");
+            var number2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Third number: ");
+            var number3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Fourth number: ");
+            var number4 = Convert.ToDouble(Console.ReadLine());
+            var average = ((number1 + number2 + number3 + number4) /4) ;
+
+            Console.WriteLine("The average of {0}, {1}, {2}, {3}, is: {4} ", number1, number2, number3, number4, average);
+
+
+
+        }
+    }
+ }
+
