81187668497cb6338461848461a50a1cd2f9e7d3
David Bass <davidbass@rtonational.com>
2022-12-14 12:40:04 -0500

    Add files via upload

---
+++ VisCheckPulse/VisCheckPulse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisCheckPulse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.WebClient client = new System.Net.WebClient();
+            client.DownloadData(new Uri("http://192.168.1.187/vischeck/process.aspx"));
+        }
+    }
+}
