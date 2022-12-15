b92cdfa8e67e648e3a17dcbcde08326d54836192
David Bass <davidbass@rtonational.com>
2022-12-15 16:11:49 -0500

    Add files via upload

---
+++ CheckAge/CheckAge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CheckAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age - ");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 30)
+            {
+                var message1 = "You look younger than ";
+                Console.WriteLine();
+                Console.Write(message1 + age);
+            }
+            else
+            {
+                var message2 = "You look older than ";
+                Console.WriteLine();
+                Console.Write(message2 + age);
+            }               
+
+
+        }
+    }
+}
