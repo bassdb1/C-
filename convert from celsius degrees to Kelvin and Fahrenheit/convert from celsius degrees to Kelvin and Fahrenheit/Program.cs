6ed177fbbe5594bb00d9de0534692675d3cfe766
David Bass <davidbass@rtonational.com>
2023-01-03 11:29:16 -0500

    Add files via upload

---
+++ convert from celsius degrees to Kelvin and Fahrenheit/convert from celsius degrees to Kelvin and Fahrenheit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace convert_from_celsius_degrees_to_Kelvin_and_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of Celsius: ");
+            var ctemp = Console.ReadLine();
+            int celsius = Convert.ToInt32(ctemp);
+
+
+            // Convert Celsious to Kelvin and Fahrenheit
+
+            var kelvin = celsius + 273;
+            var fahrenheit = ((celsius * 9 / 5) + 32);
+
+            Console.WriteLine("Kelvin = {0}", kelvin);
+            Console.WriteLine("Fahrenheit = {0}", fahrenheit);
+        }
+    }
+}
