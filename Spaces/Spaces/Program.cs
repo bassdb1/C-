971244b3960dcf49e41be96982cf6e81afdd4d27
David Bass <davidbass@rtonational.com>
2023-01-03 10:04:59 -0500

    Add files via upload

---
+++ Spaces/Spaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Spaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a digit: ");
+            var digit = Console.ReadLine();
+
+            Console.WriteLine("{0} {0} {0} {0}", digit);
+            Console.WriteLine("{0}{0}{0}{0}", digit);
+            Console.WriteLine("{0} {0} {0} {0}", digit);
+            Console.WriteLine("{0}{0}{0}{0}", digit);
+
+        }
+    }
+}
