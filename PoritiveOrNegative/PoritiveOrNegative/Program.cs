c3f823a0047869f555e5a3ff8fdf640d487c185d
David Bass <davidbass@rtonational.com>
2023-01-16 16:33:29 -0500

    Add files via upload

---
+++ PoritiveOrNegative/PoritiveOrNegative/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PoritiveOrNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Enter a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("is positive");
+            else
+                Console.WriteLine("is negative");
+        }
+    }
+}
